db3d34e2dcc17b8392511daa4fe787644200cfd3
Andrew <nion8@mail.ru>
2022-07-26 20:11:29 +0300

    conteyner1

--- chek1.go
+++ chek1.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Приложение")
 	w.Resize(fyne.NewSize(400, 320))
+
+	w.SetContent(container.NewVBox())
+
+	w.ShowAndRun()
 
 }
