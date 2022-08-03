6810ca06525bd93fb57d3fd1b1d98a653bad51db
Andrew <nion8@mail.ru>
2022-08-03 10:52:16 +0300

    check bors

--- chek1.go
+++ chek1.go
@@ -4,12 +4,17 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Приложение")
 	w.Resize(fyne.NewSize(400, 320))
+
+	checks := widget.NewCheckGroup([]string{"Check1", "Check2", "Check3"}, func() {
+
+	})
 
 	w.SetContent(container.NewVBox())
 
